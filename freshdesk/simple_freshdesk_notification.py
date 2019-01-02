44903956fc598a0b7a62717d7cc0c78f5cb703a4
Andrii <38739535+hiering@users.noreply.github.com>
2019-01-02 10:58:15 +0200

    Create simple_freshdesk_notification.py

---
+++ freshdesk/simple_freshdesk_notification.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import requests
+import json
+
+import ctypes
+
+api_key = "3hiZHXLYmGJFTBa3mtP3"
+domain = "radymo"
+password = "marakabes13"
+
+r = requests.get("https://"+ domain +".freshdesk.com/api/v2/tickets", 
+	auth = (api_key, password))
+
+response = json.loads(r.content)
+
+for i in response:
+	if i["responder_id"] == 27000113598:
+		pass
+		# notification
